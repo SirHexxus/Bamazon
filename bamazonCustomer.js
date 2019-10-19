2364cf655126ccf008793c761128ec3a1cae03dc
SirHexxus <jamesmichaelstacy@gmail.com>
2019-10-18 20:35:09 -0700

    drastically changing the structure of the program to create a more functional approach

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,114 +1,181 @@
 //  Global Constants and Variables
 //  //  Required Modules
-const mysql     = require('mysql');
-const inquirer  = require('inquirer');
-const dotenv    = require("dotenv").config();
+const mysql = require("mysql")
+const inquirer = require("inquirer")
+const dotenv = require("dotenv").config()
 
 //  //  Connection config data
 const connection = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    port     : process.env.DB_PORT,
-    database : 'bamazon',
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS
-  });
+	multipleStatements: true,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	database: "bamazon",
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS
+})
 
 //  //  Inquirer Questions
 const questions = [
-    {
-        type    : 'input',
-        name    : 'id',
-        message : 'Which Item Would You Like To Purchase? (By Item_Id)'
-    },
-    {
-        type    : 'input',
-        name    : 'amount',
-        message : 'How Many of This Item Would You Like?'
-    }
+	{
+		type: "input",
+		name: "id",
+		message: "Which Item Would You Like To Purchase? (By Item_Id)"
+	},
+	{
+		type: "input",
+		name: "amount",
+		message: "How Many of This Item Would You Like?"
+	}
 ]
 
+//  //  Global Variables
+let holding
+
 //  //  Prettifying
-const bottomBorder = "-=- =-= ".repeat(4);
+const bottomBorder = "-=- =-= ".repeat(4)
+const clear = "\n".repeat(75)
 //  - - - - - - - - - - - - - - -
 
 //  Global Function Definitions
 
 //  //  Makes query to the Database
+const makeQuery = (query) => {
+	connection.connect((err) => {
+        if(err) {
+            console.log('Connection Error:');
+            console.log(err.code);
+            console.log(err.fatal);
+            console.log(err);
+        }
+        console.log('Connection Opened!');
+	})
 
+	connection.query(query, (err, res) => {
+		if (err) {
+            console.log('Query Error:');
+			console.log(err.code);
+			console.log(err.fatal);
+			console.log(err)
+		}
+		console.log('Here are the results!\n' + JSON.stringify(res));
+	})
 
-
-const makeQuery = async (columns, table) => {
-    
-    let promise = new Promise(function(resolve, reject){
-        let columnQuery = columns.join(', ');
-        connection.query('SELECT ' + columnQuery + " FROM " + table, (err, res) => {
-            if(err) {
-                reject(err)
-            }
-            console.log(bottomBorder);
-            for(let i = 0; i < res.length; i++) {
-                for(let j = 0; j < columns.length; j++) {
-                    if(columns[j] === 'price') {
-                        console.log(columns[j] + ':\t\t$' + res[i][columns[j]]);
-                    }
-                    else {
-                        console.log(columns[j] + ':\t' + res[i][columns[j]]);
-                    }
-                }
-                console.log(bottomBorder);
-            }
-            resolve(res)
-        })
+	connection.end(err => {
+		if (err) {
+            console.log('Connection End Error:');
+			console.log(err.code);
+			console.log(err.fatal);
+			console.log(err);
+        }
+        console.log('Connection Closed!');
     })
-
-    return promise;
     
-    
+    return holding;
 }
 
-async function test(){
+// const getProducts = async (columns, table) => {
 
-    //  //  Makes Query to 'products' table
-    let query = [ "item_id", "product_name", "price" ];
-    await makeQuery( query, "products" );
-    let answers = await inquirer.prompt(questions)
-    console.log( JSON.stringify(answers, null, '  ') )
+//     let promise = new Promise(function(resolve, reject){
+//         let columnQuery = columns.join(', ');
+//         connection.query('SELECT ' + columnQuery + " FROM " + table + ";", (err, res) => {
+//             if(err) {
+//                 reject(err)
+//             }
+//             console.log(bottomBorder);
+//             for(let i = 0; i < res.length; i++) {
+//                 for(let j = 0; j < columns.length; j++) {
+//                     if(columns[j] === 'price') {
+//                         console.log(columns[j] + ':\t\t$' + res[i][columns[j]]);
+//                     }
+//                     else {
+//                         console.log(columns[j] + ':\t' + res[i][columns[j]]);
+//                     }
+//                 }
+//                 console.log(bottomBorder);
+//             }
+//             resolve(res)
+//         })
+//     })
 
-}
+//     return promise;
+
+// }
+
+// const updateStock = (table, id, num) => {
+//     connection.connect(function(err) {
+//         console.log(err.code);
+//         console.log(err.fatal);
+//         console.log(err);
+//       });
+
+//     connection.query('SELECT stock_quantity FROM ' + table + ' WHERE item_id = ?', [id], (err, res) => {
+//         if(err) {
+//             console.log(err.code);
+//             console.log(err.fatal);
+//             return console.log(err);
+//         }
+//         holding = res;
+//         console.log('This is holding: ' + JSON.stringify(holding[0]));
+//     })
+
+//     if(holding.stock_quantity > num) {
+//         let number = holding.stock_quantity - num;
+//         connection.query('UPDATE ' + table + ' SET stock_quantity = ? WHERE item_id = ?',[ number, id ], (err, res) => {
+//             if(err) {
+//                 console.log(err.code);
+//                 console.log(err.fatal);
+//                 return console.log(err);
+//             }
+//             return res;
+//         })
+//     }
+// }
+
+// async function test(){
+
+//     //  //  Makes Query to 'products' table
+//     let query = [ "item_id", "product_name", "price" ];
+//     await getProducts( query, "products" );
+//     let answers = await inquirer.prompt(questions);
+//     console.log(clear + bottomBorder);
+//     console.log( JSON.stringify( updateStock('products', answers.id, answers.amount) ) );
+
+// }
 
 //  - - - - - - - - - - - - - - -
 
 //  Events/Event Listeners
 
 //  //  Listens for Configuration Errors in `dotenv`
-if(dotenv.error) throw dotenv.error;
+if (dotenv.error) {
+    console.log(dotenv.error);
+}
 
 //  //  Connects to Bamazon Database
-connection.connect( (err) => {
-    if(err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+// connection.connect(err => {
+// 	if (err) {
+// 		console.error("error connecting: " + err.stack)
+// 		return
+// 	}
+// })
 
-})
-
-test()
+makeQuery('SELECT * FROM products;');
 
 //  //  Makes Query to 'products' table
 /* 
 let query = [ "item_id", "product_name", "price" ];
-makeQuery( query, "products" );
+getProducts( query, "products" );
 
-//  //  Prompt number 1
+
 inquirer.prompt( questions ).then( answers => {
     console.log( JSON.stringify(answers, null, '  ') )
 
 })*/
 
 //  //  Closes connection to Bamazon Database
-connection.end( (err) => {
+/* connection.end( (err) => {
     if(err) throw err;
-})
+}) */
 
 //  - - - - - - - - - - - - - - -
 
